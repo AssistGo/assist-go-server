3c0ace46615bf6ec44e5e76c78445158eb7e21b4
DNofulla <dnofulla@outlook.com>
2022-02-17 00:52:03 -0500

    Fixed Docker Script File

--- docker_script.sh
+++ docker_script.sh
@@ -10,4 +10,4 @@ echo "MONGO_USERNAME=${Username}" >> .env
 echo "MONGO_USERNAME=${Password}" >> .env
 
 docker build . -t assist-go-server
-sudo docker run -p 8080:8080 -d assist-go-server
+docker run -p 8080:8080 -d assist-go-server
