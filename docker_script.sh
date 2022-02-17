20c53be878712449c7cc583912c3a969e46d0db6
DNofulla <dnofulla@outlook.com>
2022-02-17 00:40:06 -0500

    Fixed Docker File

--- docker_script.sh
+++ docker_script.sh
@@ -6,5 +6,4 @@ read Username
 echo "What is your MongoDB Password? (Case-sensitive) "
 read Password
 
-sudo docker run -p 8080:8080 -d l9caps/assist-go-server -t -i -e MONGO_USERNAME="${Username}" \
--e MONGO_PASSWORD="${Password}"
+sudo docker run -p 8080:8080 -d assist-go-server
