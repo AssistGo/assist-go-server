439a41233c14cf69a3fa55910203312367dde914
DNofulla <dnofulla@outlook.com>
2022-04-23 00:55:00 -0400

    Fixed user endpoints

--- src/controllers/User.controller.ts
+++ src/controllers/User.controller.ts
@@ -8,7 +8,7 @@ class UserController {
   constructor() {
     this.initializeRoutes();
   }
-
+  //http://34.73.16.73:8080/users/sync
   private initializeRoutes() {
     // Test Routes
     // this.router.get(this.path + "/test/all", UserService);
@@ -18,16 +18,23 @@ class UserController {
     this.router.post(this.path + "/sync", UserService.actions.syncUserData);
 
     // User Info
-    this.router.get(this.path + "/info", UserService.actions.getUserInfo);
+    this.router.post(this.path + "/info", UserService.actions.getUserInfo);
+
     this.router.put(
       this.path + "/changeNumber",
       UserService.actions.changePhoneNumber,
     );
-    this.router.put(this.path + "/info", UserService.actions.changeUserInfo);
+
+    this.router.put(
+      this.path + "/info/:user_id",
+      UserService.actions.changeUserInfo,
+    );
+
     this.router.delete(
       this.path + "/delete",
       UserService.actions.deleteAccount,
     );
+
     this.router.put(
       this.path + "/language",
       UserService.actions.changeLanguage,
@@ -38,16 +45,21 @@ class UserController {
       this.path + "/contact",
       UserService.actions.removeContact,
     );
+
     this.router.post(this.path + "/contact", UserService.actions.createContact);
+
     this.router.put(this.path + "/contact", UserService.actions.updateContact);
+
     this.router.get(
-      this.path + "/contact/:user_id",
+      this.path + "/contact/:user_id/:contact_id",
       UserService.actions.getContact,
     );
-    this.router.post(
-      this.path + "/contact/all",
+
+    this.router.get(
+      this.path + "/contact/all/:user_id",
       UserService.actions.getAllContacts,
     );
+
     // this.router.post(
     //   this.path + "/contact/qr",
     //   UserService.actions.createContactByQRCode,
