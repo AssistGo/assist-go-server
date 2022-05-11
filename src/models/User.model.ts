ec624aae79b99b62d93fa99f6059c31e302ca1e3
DNofulla <dnofulla@outlook.com>
2022-05-11 18:18:38 -0400

    Refactored Server

--- src/models/User.model.ts
+++ src/models/User.model.ts
@@ -6,7 +6,7 @@ const User = new Schema({
   country: { type: String, unique: false, required: true }, // Country Name / Code
   countryCode: { type: String, unique: false, required: true }, // Country Phone Number Code
   phoneNumber: { type: String, unique: false, required: true }, // Pnone Number without country code
-  fullPhoneNumber: { type: String, unique: true, required: true }, // Phone Number with country code
+  fullPhoneNumber: { type: String, unique: false, required: true }, // Phone Number with country code
   fullName: { type: String, unique: false, required: false }, // Full Name
   profileImageUrl: { type: String, unique: false, required: true }, // Profile Image Url
   // language: { type: String, unique: false, required: true },
@@ -18,7 +18,7 @@ const User = new Schema({
       country: { type: String, unique: false, required: true }, // Country Name / Code
       countryCode: { type: String, unique: false, required: true }, // Country Phone Number Code
       phoneNumber: { type: String, unique: false, required: true }, // Pnone Number without country code
-      fullPhoneNumber: { type: String, unique: true, required: true }, // Phone Number with country code
+      fullPhoneNumber: { type: String, unique: false, required: true }, // Phone Number with country code
       fullName: { type: String, unique: false, required: false }, // Full Name
       profileImageUrl: { type: String, unique: false, required: true }, // Profile Image Url
       hasSimCard: { type: Boolean, unique: false, required: true },
@@ -32,7 +32,7 @@ const User = new Schema({
       country: { type: String, unique: false, required: true }, // Country Name / Code
       countryCode: { type: String, unique: false, required: true }, // Country Phone Number Code
       phoneNumber: { type: String, unique: false, required: true }, // Pnone Number without country code
-      fullPhoneNumber: { type: String, unique: true, required: true }, // Phone Number with country code
+      fullPhoneNumber: { type: String, unique: false, required: true }, // Phone Number with country code
       fullName: { type: String, unique: false, required: false }, // Full Name
       profileImageUrl: { type: String, unique: false, required: true }, // Profile Image Url
       hasSimCard: { type: Boolean, unique: false, required: true },
